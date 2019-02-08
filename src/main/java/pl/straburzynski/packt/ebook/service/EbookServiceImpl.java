3cd2b91059ad94b5173d10bf6cb72901b59acc4d
Sebastian Straburzyński <sebastian@straburzynski.pl>
2019-02-08 13:19:36 +0100

    Packt website update

--- src/main/java/pl/straburzynski/packt/ebook/service/EbookServiceImpl.java
+++ src/main/java/pl/straburzynski/packt/ebook/service/EbookServiceImpl.java
@@ -2,33 +2,27 @@ package pl.straburzynski.packt.ebook.service;
 
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import lombok.extern.java.Log;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.openqa.selenium.By;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.straburzynski.packt.ebook.config.ApplicationConfig;
 import pl.straburzynski.packt.ebook.exception.ClaimingEbookException;
-import pl.straburzynski.packt.ebook.exception.CouldNotConnectToPacktWebsiteException;
 import pl.straburzynski.packt.ebook.exception.InvalidEbookException;
 import pl.straburzynski.packt.ebook.exception.LoginFailedException;
 import pl.straburzynski.packt.ebook.model.Ebook;
 import pl.straburzynski.packt.ebook.model.ValidationMessage;
 import pl.straburzynski.packt.ebook.utils.Validator;
 import pl.straburzynski.packt.ebook.utils.WebDriverUtils;
-import us.codecraft.xsoup.Xsoup;
-
-import java.io.IOException;
 
 @Log
 @Service
 public class EbookServiceImpl implements EbookService {
 
-    private final String BOOK_TITLE_CLASS = "dotd-title";
-    private final String BOOK_DESCRIPTION_1_XPATH = "//div[contains(@class, 'dotd-main-book-summary')]/div[3]/text()";
-    private final String BOOK_DESCRIPTION_2_XPATH = "//div[contains(@class, 'dotd-main-book-summary')]/div[4]";
-    private final String BOOK_URL_XPATH = "//div[contains(@class, 'dotd-main-book-image')]/a/@href";
-    private final String BOOK_IMAGE_URL_XPATH = "//div[contains(@class, 'dotd-main-book-image')]//img/@src";
+    private final String BOOK_TITLE_CLASS = "book-top-block-info-title float-left";
+    private final String BOOK_DESC_CLASS = "book-info-bottom-indetail-text";
+    private final String BOOK_IMAGE_CLASS = "product__img";
+    private final String BOOK_URL_CLASS = "product__img-wrapper";
+
     private final String BOOK_CLAIM_BUTTON_ID = "free-learning-claim";
     private final String USER_LOGIN_PAGE_URL = "https://www.packtpub.com/login";
     private final String MY_BOOKS_PAGE_URL = "https://www.packtpub.com/account/my-ebooks";
@@ -46,24 +40,28 @@ public class EbookServiceImpl implements EbookService {
 
     @Override
     public Ebook getTodayFreeEbookDataFromPackt() {
-        try {
-            Document doc = Jsoup.connect(applicationConfig.getPacktFreeEbookUrl()).get();
-            Ebook ebook = Ebook.builder()
-                    .title(doc.getElementsByClass(BOOK_TITLE_CLASS).text())
-                    .description(prepareDescription(doc))
-                    .bookUrl(applicationConfig.getPacktBaseUrl() + Xsoup.compile(BOOK_URL_XPATH).evaluate(doc).get())
-                    .imageUrl(Xsoup.compile(BOOK_IMAGE_URL_XPATH).evaluate(doc).get())
-                    .build();
-            ValidationMessage validationMessage = Validator.validateEbook(ebook);
-            if (validationMessage.isValid()) {
-                return ebook;
-            } else {
-                String messages = validationMessage.getMessages().toString();
-                throw new InvalidEbookException(messages);
-            }
-        } catch (IOException e) {
-            throw new CouldNotConnectToPacktWebsiteException("Could not connect to Packt website");
+
+        JBrowserDriver driver = new JBrowserDriver(WebDriverUtils.settings);
+        driver.get(applicationConfig.getPacktFreeEbookUrl());
+        String imageUrl = driver.findElementByClassName(BOOK_IMAGE_CLASS).getAttribute("src");
+        driver.findElementByClassName(BOOK_URL_CLASS).click();
+
+        Ebook ebook = Ebook.builder()
+                           .title(driver.findElementByClassName(BOOK_TITLE_CLASS).getText())
+                           .description(driver.findElementByClassName(BOOK_DESC_CLASS).getText())
+                           .bookUrl(driver.getCurrentUrl())
+                           .imageUrl(imageUrl)
+                           .build();
+        driver.quit();
+
+        ValidationMessage validationMessage = Validator.validateEbook(ebook);
+        if (validationMessage.isValid()) {
+            return ebook;
+        } else {
+            String messages = validationMessage.getMessages().toString();
+            throw new InvalidEbookException(messages);
         }
+
     }
 
     @Override
@@ -108,9 +106,4 @@ public class EbookServiceImpl implements EbookService {
         }
     }
 
-    private String prepareDescription(Document doc) {
-        String desc_1 = Xsoup.compile(BOOK_DESCRIPTION_1_XPATH).evaluate(doc).get().trim();
-        String desc_2 = Xsoup.compile(BOOK_DESCRIPTION_2_XPATH).evaluate(doc).getElements().text();
-        return desc_1 + ((desc_2.isEmpty()) ? "" : "\n" + desc_2);
-    }
 }
